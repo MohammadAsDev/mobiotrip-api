56af36e86c285ba9048e4d5a78f05a7669ee657e
mohammad <mohammad@debian>
2024-06-29 22:26:57 +0300

    Modify Employees Manager to Fit My React App
    Some end-points have been added, to support some new functionalities.

--- employees_manager/urls.py
+++ employees_manager/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("publishers/" , view=PublishersView.as_view() , name="publishers"),
     path("publishers/hire/" , view=CreatePublisher.as_view() , name="create_publisher"),
     path("publishers/<int:pk>/" , view=PublisherDetails.as_view(), name="publisher_details"),
+    path("publishers/my_posts/" , view=PublisherPostsView.as_view() , name="my_posts"),
+    path("profile/" , view=AccountDetails.as_view() , name="account_view"),
 ]
--- employees_manager/views.py
+++ employees_manager/views.py
@@ -1,14 +1,22 @@
-from rest_framework.generics import ListAPIView , RetrieveUpdateDestroyAPIView , CreateAPIView
+from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
+from rest_framework import views
+from rest_framework.response import Response
 
 from django_filters.rest_framework import DjangoFilterBackend
 
 from users_manager.permissions import IsOwnerOrStaff
+from users_manager.serializers import UserSerializer
+
+from news_platform.permissions import IsPublisher
+from news_platform.models import Post
+from news_platform.serializers import PostSerializer, PostModelSerializer
+
 from .serializers import PublisherSerializer
 from .models import Publisher
 # Create your views here.
 
-class PublishersView(ListAPIView):
+class PublishersView(generics.ListAPIView):
     permission_classes = [IsAdminUser]
     queryset = Publisher.objects.all()
     serializer_class = PublisherSerializer
@@ -22,13 +30,36 @@ class PublishersView(ListAPIView):
             return selected_user_queyset
         return super().get_queryset()
 
-class CreatePublisher(CreateAPIView):
+class CreatePublisher(generics.CreateAPIView):
     permission_classes = [IsAdminUser]
     queryset = Publisher.objects.all()
     serializer_class = PublisherSerializer
 
-class PublisherDetails(RetrieveUpdateDestroyAPIView):
+class PublisherDetails(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrStaff]
     queryset = Publisher.objects.all()
     serializer_class = PublisherSerializer
 
+class AccountDetails(views.APIView):
+    def get(self, request, format=None):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+class PublisherPostsView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    permission_classes = [IsPublisher, ]
+    serializer_class = PostModelSerializer
+
+    def get_queryset(self, user):
+        return self.queryset.filter(publisher = user.id)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset(request.user))
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
